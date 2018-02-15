ab635b69d064584469da96d9b108c7bf0905e01a
Francisco Caballero <fjcero@gmail.com>
2018-02-15 18:53:54 -0300

    Get top post from Reddit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor () {
+    super();
+
+    this.state = {
+      posts: [],
+    }
+  }
+
+  componentDidMount () {
+    fetch('https://www.reddit.com/top.json')
+    .then(res => {
+      return res.json();
+    }).then(res => {
+      let posts = res.data.children.map((item) => {
+        return (
+          <div key={item.data.id}>
+            {item.data.author}
+            {item.data.url}
+            {item.data.title}
+            {item.data.subreddit_name_prefixed}
+            {item.data.num_comments}
+            {item.data.permalink}
+          </div>
+        )
+      })
+      this.setState({posts})
+      console.log({posts})
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +41,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="list">
+          {this.state.posts}
+        </div>
       </div>
     );
   }
